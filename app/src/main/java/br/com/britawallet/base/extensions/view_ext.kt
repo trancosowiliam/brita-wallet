f00b9726ca747f3363563786c21e49401c02b03d
trancosowiliam <wileleh@gmail.com>
2019-11-18 08:37:38 -0300

    feat: change layout login

--- app/src/main/java/br/com/britawallet/base/extensions/view_ext.kt
+++ app/src/main/java/br/com/britawallet/base/extensions/view_ext.kt
@@ -2,6 +2,8 @@ package br.com.britawallet.base.extensions
 
 import android.os.SystemClock
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.widget.AppCompatEditText
 
 private class SafeClickListener(
     private var defaultInterval: Int = 2300,
@@ -25,4 +27,19 @@ fun View.setSafeOnClickListener(onSafeClick: (View) -> Unit) {
     }
 
     setOnClickListener(safeClickListener)
+}
+
+fun AppCompatEditText.onActionListener(function: () -> Unit) {
+    this.setOnEditorActionListener { _, id, _ ->
+        when (id) {
+            EditorInfo.IME_ACTION_DONE,
+            EditorInfo.IME_ACTION_SEND,
+            EditorInfo.IME_ACTION_SEARCH,
+            EditorInfo.IME_ACTION_GO -> {
+                function()
+                return@setOnEditorActionListener true
+            }
+            else -> return@setOnEditorActionListener false
+        }
+    }
 }
